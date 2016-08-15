2f5f043574abc540cdafbe029d51956a6d8cc2f8
boleixiongdi <bl.jiwenlei@gmail.com>
2016-08-15 10:31:37 +0800

    init shop system

---
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -0,0 +1,36 @@
+package com.taotao.portal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taotao.portal.service.ContentService;
+
+@Controller
+public class IndexController {
+	
+	@Autowired
+	ContentService contentService;
+
+	@RequestMapping("/index")
+	public String showIndex(Model model) {
+		String adJson = contentService.getContentList();
+		model.addAttribute("ad1", adJson);
+		
+		return "index";
+	}
+	
+	
+	//produces=MediaType.TEXT_PLAIN_VALUE+";charset=utf-8"是 为了解决乱码问题
+	@RequestMapping(value="/httpclient/post", method=RequestMethod.POST,
+			produces=MediaType.TEXT_PLAIN_VALUE+";charset=utf-8")
+	@ResponseBody
+	public String testPost(String username, String password) {
+		return "username:" + username + "\tpassword:" + password;
+//		return "OK";
+	}
+}
---
+++ taotao-portal/src/main/java/com/taotao/portal/service/SearchService.java
@@ -0,0 +1,10 @@
+package com.taotao.portal.service;
+
+import com.taotao.portal.pojo.SearchResult;
+
+
+
+public interface SearchService {
+
+	SearchResult search(String queryString, int page);
+}
